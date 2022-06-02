d72089b9a6b7607951194dbb1132c8952a31a2aa
Your Name <you@example.com>
2022-06-02 17:10:27 -0300

    Final do vídeo 1.5

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-console.log(somaHorasExtras(2000, 500));
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const teste = (titulo, esperado, retornado) => {
+  if (esperado === retornado) {
+    console.log(`${titulo} passou`);
+  } else {
+    console.error(`${titulo} não passou`);
+  }
+};
+
+teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+
+teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
