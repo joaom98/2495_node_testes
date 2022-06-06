58a1e8b6f49ce15d48f5d6664ec503cd6a302d50
Your Name <you@example.com>
2022-06-06 18:06:55 -0300

    Final do vídeo 2.1

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtra
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
-    console.log(`${titulo} passou`);
-  } else {
-    console.error(`${titulo} não passou`);
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        // eslint-disable-next-line no-throw-literal
+        throw {};
+      }
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} passou!`);
+  } catch {
+    console.error(`${titulo} não passou!!!`);
   }
 };
 
-teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+teste('somaHorasExtras', () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
 
-teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+teste('calculaDesconto', () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 200);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
